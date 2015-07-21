cfd18fc06192b78af913731aed4e6163ed85968c
Neil Z. SHAO <initialneil@gmail.com>
2015-07-21 15:03:45 +0800

    remove GeneratePB.bat from PreBuilt Even
    GeneratePB.bat should be used manually.

--- caffe/src/caffe/proto/GeneratePB.bat
+++ caffe/src/caffe/proto/GeneratePB.bat
@@ -1,17 +1,11 @@
-set PROTOC=../../3rdparty/tools/protoc.exe
+cd ../../../
+set PROTOC=../3rdparty/tools/protoc.exe
 
-if exist "src/caffe/proto/caffe.pb.h" (
-    echo caffe.pb.h remains the same as before
-) else (
-    echo caffe.pb.h is being generated
-    "%PROTOC%" -I="src/caffe/proto" --cpp_out="src/caffe/proto/" "src/caffe/proto/caffe.proto"
-)
+echo caffe.pb.h is being generated
+"%PROTOC%" -I="src/caffe/proto" --cpp_out="src/caffe/proto/" "src/caffe/proto/caffe.proto"
 
-if exist "src/caffe/proto/caffe_pretty_print.pb.h" (
-    echo caffe_pretty_print.pb.h remains the same as before
-) else (
-    echo caffe_pretty_print.pb.h is being generated
-    "%PROTOC%" -I="src/caffe/proto" --cpp_out="src/caffe/proto/" "src/caffe/proto/caffe_pretty_print.proto"
-)
+echo caffe_pretty_print.pb.h is being generated
+"%PROTOC%" -I="src/caffe/proto" --cpp_out="src/caffe/proto/" "src/caffe/proto/caffe_pretty_print.proto"
 
+pause
 
